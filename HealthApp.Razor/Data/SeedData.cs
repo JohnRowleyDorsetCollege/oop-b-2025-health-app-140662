80f3030af5e17d0c5445354957cd239b246717e3
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-20 13:46:25 +0000

    seed

--- HealthApp.Razor/Data/SeedData.cs
+++ HealthApp.Razor/Data/SeedData.cs
@@ -1,11 +1,34 @@
-﻿namespace HealthApp.Razor.Data
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HealthApp.Razor.Data
 {
+    public static class HealthAppRoles
+    {
+        public const string Admin = "Admin";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
     public static class SeedData
     {
         public static async Task Initialize(IServiceProvider serviceProvider)
         {
             using (var scope = serviceProvider.CreateScope())
             {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var roles = new string[] { HealthAppRoles.Patient, HealthAppRoles.Doctor, HealthAppRoles.Admin };
+
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        Console.WriteLine($"Creating role {role}");
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+
             }
         }
     }

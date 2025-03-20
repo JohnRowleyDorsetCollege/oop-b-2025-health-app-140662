175561c72bdfc15cf1d438ec0a00fa7d30f46198
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-20 13:52:52 +0000

    sql

---
+++ HealthApp.Razor/Data/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿select * from aspnetUserRoles;
+select * from aspnetUsers;
+select * from aspnetRoles;
+
+/*
+
+delete from aspnetUserRoles;
+delete from aspnetUsers;
+delete from aspnetRoles;
+
+*/

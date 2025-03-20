8e36cf7369385a7041b13c41b7c075aad21efff7
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-20 13:31:13 +0000

    seeddata

---
+++ HealthApp.Razor/Data/SeedData.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthApp.Razor.Data
+{
+    public static class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+            }
+        }
+    }
+}
